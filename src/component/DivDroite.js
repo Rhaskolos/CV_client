d094d865c36e604d2249fabc77ed5309cb16f4d3
Rémi Girard <remi.girard54@gmail.com>
2024-03-22 09:15:48 +0100

    test

--- src/component/DivDroite.js
+++ src/component/DivDroite.js
@@ -4,7 +4,63 @@ function DivDroite()
 {
     return(
         <div className="div_droite">
-<h1 className="titre_principal">Concepteur développeur d'application</h1>
+<h1 className="titre_principal">Concepteur développeur <br></br> d'application</h1>
+<div>
+    <h2>Compétences : </h2>
+    <div>
+    <h3>Informatiques :</h3>
+    <ul>
+        <li>Développer une applicaiton sécurisée</li>
+        <li>Concevoir et développer une application sécurisée organisée en couches</li>
+        <li>Préparer le déploiement d'une applicaiton sécurisée</li>
+    </ul>
+    </div>
+    <div>
+        <h3>Données : </h3>
+        <ul>
+            <li>Prospection et recueil de données</li>
+            <li>Réalisation de tests d'expériementations</li>
+            <li>Analyse de données</li>
+        </ul>
+    </div>
+    <div>
+        <h3>Gestion de projets : </h3>
+        <ul>
+            <li>Présentation de résultats d’études aux décideurs</li>
+            <li>Travailler au sein d’une équipe</li>
+            <li>Réalisation et publication d’articles de presse</li>
+            <li>Gestion juridique, comptable et administrative d'équipes d'élus</li>
+        </ul>
+    </div>
+</div>
+<h2>Expériences professionnelles :</h2>
+<div>
+    <ul>
+        <li>2020-2022 :</li>
+    </ul>
+    <p>Animateur syndical de la Coordination Rurale pour l’animation des équipes
+départementales de Champagne-Ardenne ainsi que pour la région Grand Est</p>
+</div>
+<div>
+    <h2>Formations :</h2>
+    <ul>
+        <li>En cours (2023-2024) : </li>
+    </ul>
+    <p>Titre professionnel concepteur développeur d’applications – AFPA Pompey</p>
+    <ul>
+        <li>2018-2019 : </li>
+    </ul>
+    <p>Licence professionnelle, Agent de Développement : Agriculture et TERRitoire
+(ADATERR) - IUT Nancy-Brabois</p>
+    <ul>
+        <li>2016-2018 : </li>
+    </ul>
+    <p>DUT génie biologique, option agronomie - IUT Nancy-Brabois</p>
+    <ul>
+        <li>2015-2016 : </li>
+    </ul>
+    <p>Baccalauréat S, spécialité SVT / Lycée Notre Dame St Sigisbert - Nancy</p>
+</div>
         </div>
     );
 }
