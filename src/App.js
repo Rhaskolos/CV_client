aa372fa3addbce243d1f919a13a3440d4b57d0c7
Rémi <remi.girard54@gmail.com>
2024-03-24 23:48:04 +0100

    correction import dans app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import "./css/app.css";
 import { useGlobalState } from "./globalStates";
 import Page from "./component/Page";
@@ -11,6 +11,7 @@ function App() {
     useEffect(() => {
 
         loadData();
+
     }, [globalStates.currentLanguage]);
 
 
